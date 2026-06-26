1be9321c612144511d553ced1b81b35aba8a00e1
Leon Pllana <leon.pllana@lecom-ks.com>
2026-06-26 14:32:53 +0200

    Remove unused development files and type definitions from Next.js build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fKXXU8r1sEHb4Zsj+CGpStMn3QqKn4fTU1Iz4av2SQg=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1m-tfll._.css","static/chunks/node_modules_1ku_hyc._.js","static/chunks/components_theme-provider_tsx_0au3e9c._.js"],
-    source: "dynamic"
-});
