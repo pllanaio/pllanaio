2039a06377798a938880f8c4ecdb2397caa8b6d1
Leon Pllana <135324050+pllanaio@users.noreply.github.com>
2026-07-22 05:30:04 +0200

    Add Google site verification meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="jTCUyd6R5sUUJdLLn4D38fSSGDaICGwtNTJY83jCh2Y"
+      />
+    </>
+  );
+}
